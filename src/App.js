7b78ff6b5d18ccfae8faf3b6c019baef7cb93d5f
Ankit Kapoor <ankitkapoor8437@gmail.com>
2023-08-08 12:29:40 +0530

    Bug fix!

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
-import Counter from './components/Counter';
-// import Base from './components/UI/Base';
+// import Counter from './components/Counter';
+import Base from './components/UI/Base';
+import { useSelector } from 'react-redux';
 
 function App() {
+  const count = useSelector((c) => c.counter.value);
   return (
     <div className="App">
-      {/* <Base/> */}
-      <Counter/>
+      <h2>{count}</h2>
+      <Base />
+      {/* <Counter/> */}
     </div>
   );
 }
--- src/components/UI/Base.jsx
+++ src/components/UI/Base.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import Child from './Child'
+import { useSelector } from 'react-redux'
 
 const Base = () => {
+  const count = useSelector((c) => c.counter.value);
   return (
     <div>
-        <Child/>
+      <h2>{count}</h2>
+      <Child />
     </div>
   )
 }
